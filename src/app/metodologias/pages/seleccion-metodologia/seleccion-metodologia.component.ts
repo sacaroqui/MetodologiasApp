b20d07c75fc2fc3686d61aab0d760e6779bcca90
SamirCaro <sacaroqui@gmail.com>
2023-09-09 17:03:54 -0500

    Ajustes finales de la aplicacion

--- src/app/metodologias/pages/seleccion-metodologia/seleccion-metodologia.component.ts
+++ src/app/metodologias/pages/seleccion-metodologia/seleccion-metodologia.component.ts
@@ -17,52 +17,52 @@ export class SeleccionMetodologiaComponent implements OnInit {
       titulo:'¿Qué porcentaje de proyectos con enfoque ágil ha desarrollado la organización?'
     },
     {
-      titulo:'¿La organización cuenta con un entorno de trabajo colaborativo?'
+      titulo:'¿En qué porcentaje la organización cuenta con un entorno de trabajo colaborativo?'
     },
     {
-      titulo:'¿La organización cuenta con posibles patrocinadores de proyectos que entienden y apoyan el uso de un enfoque ágil?'
+      titulo:'¿En qué porcentaje la organización cuenta con posibles patrocinadores de proyectos que entienden y apoyan el uso de un enfoque ágil?'
     },
     {
-      titulo:'¿Los patrocinadores y representantes del negocio confían en que los equipos de proyectos pueden transformar sus necesidades en un producto/servicio exitoso, con retroalimentación continua en ambas direcciones?'
+      titulo:'¿En qué porcentaje los patrocinadores y representantes del negocio confían en que los equipos de proyectos pueden transformar sus necesidades en un producto/servicio exitoso, con retroalimentación continua en ambas direcciones?'
     },
     {
-      titulo:'¿La organización da autonomía a sus equipos de proyecto para tomar sus propias decisiones sobre como emprender el trabajo?'
+      titulo:'¿En qué porcentaje la organización da autonomía a sus equipos de proyecto para tomar sus propias decisiones sobre como emprender el trabajo?'
     },
     {
-      titulo:' ¿La organización está dispuesta a aceptar modificaciones periódicas a las estimaciones de tiempo y costo debido a la comprensión del producto por parte del equipo?'
+      titulo:' ¿En qué porcentaje la organización está dispuesta a aceptar modificaciones periódicas a las estimaciones de tiempo y costo debido a la comprensión del producto por parte del equipo?'
     },
     {
-      titulo:'¿La organización está dispuesta a asignar los recursos al proyecto con disponibilidad del 100% y eliminar la autoridad del jefe funcional?'
+      titulo:'¿En qué porcentaje la organización está dispuesta a asignar los recursos al proyecto con disponibilidad del 100% y eliminar la autoridad del jefe funcional?'
     },
     {
-      titulo:'¿La organización está dispuesta a dar prioridad a las funcionalidades que a la exigencia y rigurosidad de una documentación detallada?'
+      titulo:'¿En qué porcentaje la organización está dispuesta a dar prioridad a las funcionalidades que a la exigencia y rigurosidad de una documentación detallada?'
     },
     {
-      titulo:'¿La organización acepta obtener valor rápido y de manera incremental en vez de obtener el resultado completo al finalizar el proyecto?'
+      titulo:'¿En qué porcentaje la organización acepta obtener valor rápido y de manera incremental en vez de obtener el resultado completo al finalizar el proyecto?'
     },
     {
-      titulo:'Generalmente ¿Cuál es el tamaño del equipo principal de los proyectos?'
+      titulo:'¿En qué rango de porcentaje la organización está dispuesta a obtener logro y generación de valor en tiempos cortos e iterativos?'
     },
     {
-      titulo:'¿La organización cuenta con funcionarios con nivel de experiencia y habilidades para desempeñar los roles del equipo principal? (Scrum Master, Team, Product Owner)'
+      titulo:'¿En qué porcentaje la organización cuenta con funcionarios con nivel de experiencia y habilidades para desempeñar los roles del equipo principal? (Scrum Master, Team, Product Owner)'
     },
     {
       titulo:'¿Qué porcentaje de los funcionarios ha trabajado en proyectos con enfoque ágil?'
     },
     {
-      titulo:'¿Los equipos de proyecto tendrán acceso diario a por lo menos un representante del negocio/cliente con el fin de hacer preguntas y obtener retroalimentación?'
+      titulo:'¿En qué porcentaje los equipos de proyecto tendrán acceso diario a por lo menos un representante del negocio/cliente con el fin de hacer preguntas y obtener retroalimentación?'
     },
     {
-      titulo:'¿Es factible que los miembros de un equipo de proyecto se encuentren en una misma ubicación?'
+      titulo:'¿En qué rango de porcentaje se encuentra defindo el alcance, tiempo y costo del proyecto?'
     },
     {
       titulo:'¿Qué porcentaje de requisitos acepta la organización que puedan cambiar o ser descubiertos mensualmente?'
     },
     {
-      titulo:'¿La organización acepta que el producto o servicio pueda ser construido y evaluado por productos mínimos viables (MVP)?'
+      titulo:'¿En qué porcentaje la organización acepta que el producto o servicio pueda ser construido y evaluado por productos mínimos viables (MVP)?'
     },
     {
-      titulo:' ¿La organización acepta que las funcionalidades del producto o servicio puedan ser añadidas a través de las iteraciones?'
+      titulo:' ¿En qué porcentaje la organización acepta que las funcionalidades del producto o servicio puedan ser añadidas a través de las iteraciones?'
     }
 
 
@@ -110,23 +110,23 @@ export class SeleccionMetodologiaComponent implements OnInit {
   
 
   miFormulario:FormGroup= this.fb.group({
-    pregunta1:['2',[Validators.required]],
-    pregunta2:['2',[Validators.required]],
-    pregunta3:['2',[Validators.required]],
-    pregunta4:['2',[Validators.required]],
-    pregunta5:['2',[Validators.required]],
-    pregunta6:['2',[Validators.required]],
-    pregunta7:['2',[Validators.required]],
-    pregunta8:['2',[Validators.required]],
-    pregunta9:['2',[Validators.required]],
-    pregunta10:['2',[Validators.required]],
-    pregunta11:['2',[Validators.required]],
-    pregunta12:['2',[Validators.required]],
-    pregunta13:['2',[Validators.required]],
-    pregunta14:['2',[Validators.required]],
-    pregunta15:['2',[Validators.required]],
-    pregunta16:['2',[Validators.required]],
-    pregunta17:['2',[Validators.required]],
+    pregunta1:['',[Validators.required]],
+    pregunta2:['',[Validators.required]],
+    pregunta3:['',[Validators.required]],
+    pregunta4:['',[Validators.required]],
+    pregunta5:['',[Validators.required]],
+    pregunta6:['',[Validators.required]],
+    pregunta7:['',[Validators.required]],
+    pregunta8:['',[Validators.required]],
+    pregunta9:['',[Validators.required]],
+    pregunta10:['',[Validators.required]],
+    pregunta11:['',[Validators.required]],
+    pregunta12:['',[Validators.required]],
+    pregunta13:['',[Validators.required]],
+    pregunta14:['',[Validators.required]],
+    pregunta15:['',[Validators.required]],
+    pregunta16:['',[Validators.required]],
+    pregunta17:['',[Validators.required]],
 
   })
 
